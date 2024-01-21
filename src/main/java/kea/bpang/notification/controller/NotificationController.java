1939c4711c9dd6137153541032ca7fbabc4363d9
SeungHyeon <namsh1125@naver.com>
2024-01-21 13:55:50 +0900

    [Feat] API 보안 설정

--- src/main/java/kea/bpang/notification/controller/NotificationController.java
+++ src/main/java/kea/bpang/notification/controller/NotificationController.java
@@ -6,6 +6,7 @@ import kea.bpang.notification.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class NotificationController {
 
     private final EmailService emailService;
 
+    @PreAuthorize("isAuthenticated()")
     @Operation(summary = "이메일 전송", description = "특정 상황이 발생했을 때 정보를 전달하고자 할 때 사용합니다.")
     @PostMapping("/email")
     public ResponseEntity<String> sendEmailNotification(@RequestBody EmailNotificationDto dto) {
