f4a6ad0df3c6544ea7558594797c6e8991723ec7
SeungHyeon <namsh1125@naver.com>
2024-01-27 19:00:30 +0900

    [Chore] 자바독 위치 변경

--- src/main/java/kea/bpang/notification/service/EmailService.java
+++ src/main/java/kea/bpang/notification/service/EmailService.java
@@ -3,5 +3,12 @@ package kea.bpang.notification.service;
 import kea.bpang.notification.dto.EmailNotificationDto;
 
 public interface EmailService {
-    Boolean sendMessage(EmailNotificationDto dto) throws Exception;
+
+    /**
+     * 알림 전송 (이메일 전송)
+     *
+     * @param dto 어떤 내용을 누구에게 보낼지에 관한 dto
+     * @return 이메일 전송 여부
+     */
+    Boolean sendMessage(EmailNotificationDto dto);
 }
--- src/main/java/kea/bpang/notification/service/EmailServiceImpl.java
+++ src/main/java/kea/bpang/notification/service/EmailServiceImpl.java
@@ -20,12 +20,6 @@ public class EmailServiceImpl implements EmailService {
         this.emailSender = emailSender;
     }
 
-    /**
-     * 알림 전송 (이메일 전송)
-     *
-     * @param dto 어떤 내용을 누구에게 보낼지에 관한 dto
-     * @return 이메일 전송 여부
-     */
     @Override
     public Boolean sendMessage(EmailNotificationDto dto) {
         try {
